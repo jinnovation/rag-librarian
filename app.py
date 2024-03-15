a82389b17f67b8cf9364b17d38ef8eb09740aa64
Jonathan Jin <me@jonathanj.in>
2024-03-15 12:23:26 -0400

    implement download logic

--- app.py
+++ app.py
@@ -1,30 +1,35 @@
-import streamlit as st
+import os
+import pathlib
+import shutil
 import time
+
+import requests
+import streamlit as st
 
 LIBRARY: dict[str, str] = {
     "Pride and Prejudice": "https://www.gutenberg.org/ebooks/1342.epub.noimages",
     "Les Miserables": "https://www.gutenberg.org/ebooks/135.epub.noimages",
 }
 
+LIBRARY_PATH: pathlib.Path = pathlib.Path.cwd() / "library"
+
 st.title("RAG Librarian")
 
-st.multiselect(
+choices = st.multiselect(
     ":book: Choose your books",
     LIBRARY.keys(),
     default=LIBRARY.keys(),
 )
 
-
 if st.button("Download books"):
-    download_progress_bar = st.progress(
-        0, text="Downloading books from Project Gutenberg..."
-    )
-    for percent_complete in range(100):
-        time.sleep(0.01)
-        download_progress_bar.progress(
-            percent_complete + 1, text="Downloading books from Project Gutenberg..."
-        )
+    shutil.rmtree(LIBRARY_PATH)
+    LIBRARY_PATH.mkdir(parents=True, exist_ok=True)
+    with st.spinner("Downloading..."):
+        for choice in choices:
+            url = LIBRARY[choice]
+            fname = url.rsplit("/", 1)[1]
+            dest_fname: str = os.path.splitext(fname)[0]
+            r = requests.get(url, allow_redirects=True)
+            (LIBRARY_PATH / dest_fname).open("wb").write(r.content)
 
-
-x = st.slider("Select a value")
-st.write(x, "squared is", x * x)
+    st.success("Done downloading!")
